ab3b2aba42174c8938fa143645bdbe3c9113faf7
Sagar Siwakoti <siwakotisagar8@gmail.com>
2020-04-07 19:05:25 +0545

    passing functions as arguments

--- learn.js
+++ learn.js
@@ -163,9 +163,36 @@ console.log(obj.city);
 
 //Passing functions as arguments
 
-var years=[1990,1999,1997,1998,1991];
+var years=[1990,2006,1997,1998,1991];
 
 function arrayCalc(arr,fn){
     var arrRes = [];
-    for (var1 )
+    for (var i= 0; i < arr.length;i++){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
 }
+
+function calculateAge(el){
+    return 2020- el;
+}
+
+function isFullAge(el){
+    return el>=18;
+}
+
+function maxHeartRate(el){
+    if (el>=18 && el<= 81){
+      return Math.round (206.9-(0.67*el));
+    }else{
+        return -1;
+    }
+}
+
+var ages =arrayCalc(years,calculateAge);
+var fullAges= arrayCalc(ages,isFullAge);
+var rates = arrayCalc(ages,maxHeartRate)
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
